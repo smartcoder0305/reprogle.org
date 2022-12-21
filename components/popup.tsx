4b5e625ece2aef996b081f323a79891ef0f4bda6
smartcoder0305 <stunningcoder7@gmail.com>
2022-12-21 08:59:30 -0600

    Update popup.tsx

--- components/popup.tsx
+++ components/popup.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Popup({ ...props }) {
   const router = useRouter();
