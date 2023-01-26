3aa9e6844481b9563f6326df71772b3c32237e90
smartcoder0305 <stunningcoder7@gmail.com>
2023-01-26 21:53:26 +0000

    Working on Angular complete rewrite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {
-      colors: {
-        "gradient-orange": "#cc8e32",
-        "gradient-purple": "#a27cf2",
-        "dark-base-blue-100": "#010b1f",
-        "dark-base-blue-200": "#021740",
-      },
-      fontFamily: {
-        body: ["Montserrat", "sans-serif"],
-        heading: ["Playfair Display", "serif"],
-      },
-    },
+    extend: {},
   },
-  plugins: [require("@headlessui/tailwindcss")],
+  plugins: [],
 };
