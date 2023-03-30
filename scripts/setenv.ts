ec31b913ff984ab44d900fe0a99fea8670e4c982
smartcoder0305 <stunningcoder7@gmail.com>
2023-03-29 22:42:42 -0500

    Add support for Docker to move from Vercel to Google Cloud Run

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -1,7 +1,9 @@
 import { writeFile } from 'fs';
+import * as process from 'process';
 
 // Check if the NODE_ENV env variable is set to prod
 const isProduction = process.env['NODE_ENV'] === 'prod';
+const isGitHubCI = process.env['CI'] === 'true';
 
 // If we're using a prod environment, we write to the environment.prod.ts file. Otherwise, write to the environment.ts file
 const targetPath = isProduction
@@ -13,14 +15,19 @@ if (isProduction) {
   console.log(
     'Currently using a production environment. Setting environment variables as such'
   );
-} else {
+} else if (isGitHubCI) {
   console.log(
-    'Running application in development mode. Angular will handle migrating environment variables, assuming environment.dev.ts has been configured properly'
+    'Application is running in GitHub Actions. GitHub Actions combined with Angular will set the proper testing environment variables'
   );
   process.exit(0);
+} else {
+    console.log(
+      'Running application in development mode. Angular will handle migrating environment variables, assuming environment.dev.ts has been configured properly'
+    );
+    process.exit(0);
 }
 
-// Create the contents of the environment file using environment variables set via our host (Which is Vercel)
+// Create the contents of the environment file using environment variables set via our host (Which is Google Cloud Run)
 const environmentFileContent = `
 export const environment = {
    production: ${isProduction},
